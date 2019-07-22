4cccdec61cf5beac716b90477c76655226c2430e
liujie <liu_anxin@163.com>
2019-07-22 16:35:49 +0800

    fix

--- src/main/java/com/github/liuanxin/api/model/DocumentInfo.java
+++ src/main/java/com/github/liuanxin/api/model/DocumentInfo.java
@@ -1,5 +1,6 @@
 package com.github.liuanxin.api.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Accessors(chain = true)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class DocumentInfo {
 
     private List<DocumentParam> tokenList;
