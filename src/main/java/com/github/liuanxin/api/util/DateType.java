8bedc62f361bbcb554ca01f63fa0c32947bd4be9
liujie <liu_anxin@163.com>
2020-01-02 09:56:26 +0800

    fix

--- src/main/java/com/github/liuanxin/api/util/DateType.java
+++ src/main/java/com/github/liuanxin/api/util/DateType.java
@@ -2,20 +2,22 @@ package com.github.liuanxin.api.util;
 
 public enum DateType {
 
-    YYYY_MM_DD_HH_MM_SS("yyyy-MM-dd HH:mm:ss"),
-    MM_DD_YYYY_HH_MM_SS("MM/dd/yyyy HH:mm:ss"),
     YYYY_MM_DD_HH_MM_SS_SSS("yyyy-MM-dd HH:mm:ss SSS"),
+    YYYY_MM_DD_HH_MM_SS("yyyy-MM-dd HH:mm:ss"),
     MM_DD_YYYY_HH_MM_SS_SSS("MM/dd/yyyy HH:mm:ss SSS"),
+
+    MM_DD_YYYY_HH_MM_SS("MM/dd/yyyy HH:mm:ss"),
     YYYY_MM_DD("yyyy-MM-dd"),
     YYYY__MM__DD("yyyy/MM/dd"),
     MM_DD_YYYY("MM/dd/yyyy"),
-    YYYY_MM("yyyy-MM"),
+
     YYYY_MM_DD_HH_MM("yyyy-MM-dd HH:mm"),
     YYYY_MM_DD_AP("yyyy-MM-dd a"),
+    YYYY_MM("yyyy-MM"),
 
     CN_YYYY_MM_DD_HH_MM_SS("yyyy年MM月dd日 HH时mm分ss秒"),
-    CN_YYYY_MM_DD_HH("yyyy年MM月dd日 HH点"),
     CN_YYYY_MM_DD_HH_MM("yyyy年MM月dd日 HH点mm分"),
+    CN_YYYY_MM_DD_HH("yyyy年MM月dd日 HH点"),
     CN_YYYY_MM_DD("yyyy年MM月dd日"),
 
     YYYYMMDDHHMMSSSSS("yyyyMMddHHmmssSSS"),
